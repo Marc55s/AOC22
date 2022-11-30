2fdbadc9cc0a2970f1711e3241ef3dd52385575f
Marc <marc.schoenig@gmail.com>
2022-11-30 19:05:18 +0100

    Initial commit

---
+++ src/ms/aoc/Day1.java
@@ -0,0 +1,16 @@
+package ms.aoc;
+
+import java.util.List;
+
+public class Day1 extends Day{
+
+    public Day1() {
+        super("1");
+    }
+
+    @Override
+    void solve(List<String> input) {
+        
+
+    }
+}
---
+++ src/ms/aoc/Main.java
@@ -0,0 +1,7 @@
+package ms.aoc;
+
+public class Main {
+    public static void main(String[] args) {
+        new Day1();
+    }
+}
