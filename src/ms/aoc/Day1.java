4b939df54a124d1a356840e20f8df61d9e570f68
Marc <marc.schoenig@gmail.com>
2022-12-01 17:41:13 +0100

    Day 1

--- src/ms/aoc/Day1.java
+++ src/ms/aoc/Day1.java
@@ -1,5 +1,7 @@
 package ms.aoc;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Day1 extends Day{
@@ -10,7 +12,20 @@ public class Day1 extends Day{
 
     @Override
     void solve(List<String> input) {
-        
-
+        int cal = 0;
+        int max = 0;
+        ArrayList<Integer> sumCals = new ArrayList<>();
+        for (String s : input) {
+            if (!s.isEmpty()) {
+                cal += Integer.parseInt(s);
+            } else {
+                max = Math.max(max, cal);
+                sumCals.add(cal);
+                cal = 0;
+            }
+        }
+        var p2= sumCals.stream().sorted(Collections.reverseOrder()).limit(3).mapToInt(i -> i).sum();
+        System.out.printf("Part 1: %d%n",max);
+        System.out.printf("Part 2: %d%n",p2);
     }
 }
